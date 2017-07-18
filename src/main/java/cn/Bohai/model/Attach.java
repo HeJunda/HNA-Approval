bdc045096ce039c2a7a9b783693973d54b4c1035
HeJunda <2278533613@qq.com>
2017-07-18 13:52:07 +0800

    更新

---
+++ src/main/java/cn/Bohai/model/Attach.java
@@ -0,0 +1,28 @@
+package cn.Bohai.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Jaye on 2017/5/13.
+ */
+public class Attach implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户ID
+     */
+    private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+    
+    
+     
+}
+
