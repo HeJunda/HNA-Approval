03662c7daa2d659376185e2b592ed47b1b29f64b
DJT520818 <847280982@qq.com>
2017-08-07 10:49:30 +0800

    dsf

--- src/main/resources/static/js/examine.js
+++ src/main/resources/static/js/examine.js
@@ -114,230 +114,232 @@ var vm=new Vue({
 			});
 				this.showDom=!this.showDom;
 			},
-			showTip:function(){
-				this.showLayer=!this.showLayer;
-				var _this=this;
-				var taskId=_this.digital.taskid;
-				var user=getCookie('userid')
-				axios.get("/workflow/getAwaitDetail",{params:{userid:user,taskid:taskId}}).then(function(response){
-					_this.aclist=response.data;
-					_this.list=JSON.parse(_this.aclist[0].actionlist)
-					console.log(_this.list);
-				}).catch(function(error){
-				    console.log(error);
-				});
-			},
-			hideDiv:function(){
-				this.showLayer=!this.showLayer;
-			},
-			showIcon:function(name){
-				this.showLayer=!this.showLayer;
-		        this.activeName = name.name;
-		        this.status=name.name;
-		        this.parms=name;
-		        var names=name.value;
-		        var taskId=this.digital.taskid;
-		        /*弹出框(一)*/
-		        if(name.comeback!='0' && name.comeback!=""){
-		        	this.comeback=name.comeback;
-		        	var html='';
-		        	if(this.comeback==1){
-		        		html='<div class="comeback"><p><input type="radio" checked="true" name="Comeback" value="0">下一步按原流程重新提交逐级审批</p><p><input type="radio" name="Comeback" value="1">下一步直接提交至本节点</p><p><input type="radio" name="Comeback" value="2">由下一步处理人选择提交</p></div>';
-		        	}else if(this.comeback==2){
-		        		html='<div class=""><p><input type="radio" name="Comeback" value="0">下一步按原流程重新提交逐级审批</p><p><input type="radio" checked="true" name="Comeback" value="1">下一步直接提交至本节点</p><p><input type="radio" name="Comeback" value="2">由下一步处理人选择提交</p></div>';
-		        	}else if(this.comeback==3){
-		        		html=html='<div class=""><p><input type="radio" name="Comeback" value="0">下一步按原流程重新提交逐级审批</p><p><input type="radio" name="Comeback" value="1">下一步直接提交至本节点</p><p><input type="radio" checked="true" name="Comeback" value="2">由下一步处理人选择提交</p></div>';
-		        	}
-		        	var index=layer.open({
-		        	    content: html,
-		        	    btn: ['确定','取消'],
-		        	    yes: function(index){
-		        	    	var check=document.querySelectorAll("input[name='Comeback']");
-		        	    	console.log(check)
-		        	    	for(var i=0;i<check.length;i++){
-		        	    		if(check[i].checked){
-		            	    		this.comeback=check[i].value;
-		            	    	}	
-		        	    	}
-		        	    	layer.close(index);
-		        	      },
-		        	    no: function(index){
-		        	    	$(".status-text").text(" ")
-		        	    }
-		        	  });   	
-		        }else{
-		        	this.comeback=name.comeback;
-		        } 
-		        /*人物弹出框(二)*/
-		        if(name.type==2||name.type==3||name.type==4||name.type==7||name.type==9){
-		        	document.querySelector(".person-item").style.display="block";
-		        	console.log(name.type)
-		        	var _this=this;
-		        	if(name.type==2){
-		        		var user=getCookie('userid')
-		        		axios.get("/workflow/getNextNode",{params:{userid:user,actionvalue:names,taskid:taskId}}).then(function(response){
-		        			console.log(response.data)
-		            		_this.noname=response.data.nodename
-		            		console.log(_this.noname)
-		            	},function(err){
-		            		console.log(err)
-		            	})
-		        	}else{
-		            	_this.noname=""
-		        	}
-		        }else{
-		        	document.querySelector(".person-item").style.display="none";
-		        }          
-			},
-			/*点击选人*/
-			showUser:function(parame){
-				this.showuser=!this.showuser;
-				var taskId=this.digital.taskid;
-				var parames=parame.value;
-				var paramename=parame.nodename;
-				var parm=this.parms.type;
-				var names=this.parms.value;
-				var instance=this.digital.instanceid;
-				var noname=this.noname;
-				
-				var _this=this
-				var user=getCookie('userid')
-				axios.get('/workflow/selectPerson',{params:{userid:user,start:0,taskid:taskId,actionvalue:names,nodename:noname,actiontype:parm,instanceid:instance}}).then(function(response){
-					_this.person=response.data;
-					var personList=[];
-					_this.person.forEach(function(i,index,array){
-						var arrUser=i.userid
-						personList.push(arrUser);
-					})
-					
-					$(".content ul").on("click",$(".content li"),function(ev){
-						var ev = ev || window.event;
-						var target = ev.target || ev.srcElement;
-						if(target.nodeName.toLowerCase() == "li"){
-							if ($(target).children().hasClass('bg')) {
-								$(target).children('.bg').remove();
-								$(target).removeAttr('data-id');
-							}else{
-								var lis=document.createElement('span')
-								lis.setAttribute("class","bg");
-								lis.innerHTML="√";
-								if(target.nodeName.toLowerCase() == "li"){
-									var ind=$(".content li").index($(target));
-									$(target).append(lis).attr('data-id',personList[ind]);
-								}
-							}
-						}
-					})
-				},function(err){
-					console.log(err)
-				});
-			},
-			/*点击确定*/
-			showok:function(){
-				this.showuser=!this.showuser;
-				var _this=this;
-				var lis=$(".content li")
-				var personArr=[];
-				for(var i=0;i<lis.length;i++){
-					if(lis.eq(i).attr('data-id')!=undefined){
-						personArr.push(lis.eq(i).attr('data-id'));
-					}
-				}
-				_this.personStr=personArr.join(",")
-				//console.log(_this.personStr)
-				if($(".bg").length=="0"){
-					layer.open({
-					    content: '请添加选择人',
-					    skin: 'msg',
-					    style: 'background-color:#ccc; color:#fff; border:none;',
-					    time: 1
-					});
-				}
-			},
+		showTip:function(){
+			this.showLayer=!this.showLayer;
+			var _this=this;
+			var taskId=_this.digital.taskid;
+			var user=getCookie('userid')
+			axios.get("/workflow/getAwaitDetail",{params:{userid:user,taskid:taskId}}).then(function(response){
+				_this.aclist=response.data;
+				_this.list=JSON.parse(_this.aclist[0].actionlist)
+				console.log(_this.list);
+			}).catch(function(error){
+			    console.log(error);
+			});
+		},
+		hideDiv:function(){
+			this.showLayer=!this.showLayer;
+		},
+		/*处理*/
+		showIcon:function(name){
+			var _this=this;
+			this.showLayer=!this.showLayer;
+	        this.activeName = name.name;
+	        this.status=name.name;
+	        this.parms=name;
+	        var names=name.value;
+	        var taskId=this.digital.taskid;
+	        /*弹出框(一)*/
+	        if(name.comeback!='0' && name.comeback!=""){
+	        	this.comeback=name.comeback;
+	        	var html='';
+	        	if(this.comeback==1){
+	        		html='<div class="comeback"><p><input type="radio" checked="true" name="Comeback" value="0">下一步按原流程重新提交逐级审批</p><p><input type="radio" name="Comeback" value="1">下一步直接提交至本节点</p><p><input type="radio" name="Comeback" value="2">由下一步处理人选择提交</p></div>';
+	        	}else if(this.comeback==2){
+	        		html='<div class=""><p><input type="radio" name="Comeback" value="0">下一步按原流程重新提交逐级审批</p><p><input type="radio" checked="true" name="Comeback" value="1">下一步直接提交至本节点</p><p><input type="radio" name="Comeback" value="2">由下一步处理人选择提交</p></div>';
+	        	}else if(this.comeback==3){
+	        		html='<div class=""><p><input type="radio" name="Comeback" value="0">下一步按原流程重新提交逐级审批</p><p><input type="radio" name="Comeback" value="1">下一步直接提交至本节点</p><p><input type="radio" checked="true" name="Comeback" value="2">由下一步处理人选择提交</p></div>';
+	        	}
+	        	var index=layer.open({
+	        	    content: html,
+	        	    btn: ['确定','取消'],
+	        	    yes: function(index){
+	        	    	 var check=document.querySelectorAll("input[name='Comeback']");
+	        	    	 console.log(check)
+	        	    	 for(var i=0;i<check.length;i++){
+	        	    		 if(check[i].checked){
+	            	    		 this.comeback=check[i].value;
+	            	    	 }	
+	        	    	 }
+	        	    	 layer.close(index);
+	        	    },
+	        	    no: function(){
+	        	    	_this.status=""
+	        	    }
+	        	  });   	
+	        }else{
+	        	this.comeback=name.comeback;
+	        }
+	        /*人物弹出框(二)*/
+	        if(name.type==2||name.type==3||name.type==4||name.type==7||name.type==9){
+	        	document.querySelector(".person-item").style.display="block";
+	        	console.log(name.type)
+	        	var _this=this;
+	        	if(name.type==2){
+	        		var user=getCookie('userid')
+	        		axios.get("/workflow/getNextNode",{params:{userid:user,actionvalue:names,taskid:taskId}}).then(function(response){
+	        			console.log(response.data)
+	            		_this.noname=response.data.nodename
+	            		console.log(_this.noname)
+	            	},function(err){
+	            		console.log(err)
+	            	})
+	        	}else{
+	            	_this.noname=""
+	        	}
+	        }else{
+	        	document.querySelector(".person-item").style.display="none";
+	        } 
+		},
+		/*点击选人*/
+		showUser:function(parame){
+			this.showuser=!this.showuser;
+			var taskId=this.digital.taskid;
+			//var parames=parame.value;
+			//var paramename=parame.nodename;
+			var parm=this.parms.type;
+			var names=this.parms.value;
+			var instance=this.digital.instanceid;
+			var noname=this.noname;
 			
-			/*常用语*/
-			lane:function(){
-				var _this=this;
-				var user=getCookie('userid');
-				axios.get("/workflow/getCommonlanguage",{params:{userid:user}}).then(function(response){
-        			console.log(response.data)
-            		/*_this.language=response.data
-            		console.log(_this.language)*/
-        			var html='';
-        			$.each(response.data,function(i,val){
-        				html+='<option class="option" value="'+response.data[i].phrase+'">'+response.data[i].phrase+'</option>'
-        			});
-        				var L=layer.open({
-        					type: 1,
-        					anim: 'up',
-        					style: 'position:fixed; bottom:0; left:0; width: 100%; height: 200px; padding:10px 0; border:none;box-shadow: 0;',
-        					content: html+"<p class='option'>取消</p>"
-        			})
-        			$(document).on("click","option",function(){  
-        				var value=$(this).val()
-        				console.log(value)
-        				$("#sele").val(value)
-        				layer.close(L)
-        			}); 
-    				$(document).on("click","p",function(){  
-        				layer.close(L)
-        			}); 
-            		
-            	},function(err){
-            		console.log(err)
-            	})
-			},
-			/*点击提交*/
-			submit:function(){
-				/*if(this.opinion=="" || this.activeName==""){
-					layer.open({
-					    content: '输入不能为空',
-					    skin: 'msg',
-					    style: 'background-color:#ccc; color:#fff; border:none;',
-					    time: 3 
-					  });
-				}*/
-				var _this=this;
-				var str=this.opinion;
-				var parm=this.parms;
-				var comebacks=this.comeback;
-				console.log(comebacks)
-				var formtypes=this.digital.formtype;
-				var taskId=this.digital.taskid;
-				var personStrs=this.personStr;
-				var nonames=this.noname;
-				console.log(nonames)
-				var user=getCookie('userid')
-				axios.post("/workflow/processProcessing",{userid:user,taskid:taskId,nextopermap:nonames,actionname:parm.name,remark:str,actiontype:parm.type,actionvalue:parm.value,comeback:comebacks,formtype:formtypes,receiveuserids:personStrs}).then(function(response){
-					console.log(response.data)
-					console.log(response.data[0].code)
-					if(response.data[0].code=='1'){
-						layer.open({
-						    content: '提交成功',
-						    skin: 'msg',
-						    style: 'background-color:#ccc; color:#fff; border:none;',
-						    time: 3 
-						  });
-						window.location.href='agency.html';
-					}else{
-						layer.open({
-						    content: response.data[0].result,
-						    style: 'background-color:#ccc; color:#fff; border:none;',
-						    skin: 'msg',
-						    time: 3 
-						  });
-					}
-				}).catch(function(response){
-					console.log(response)
-					layer.open({
-					    content: response.result,
-					    style: 'background-color:#ccc; color:#fff; border:none;',
-					    skin: 'msg',
-					    time: 3 
-					  });
+			var _this=this
+			var user=getCookie('userid')
+			axios.get('/workflow/selectPerson',{params:{userid:user,start:0,taskid:taskId,actionvalue:names,nodename:noname,actiontype:parm,instanceid:instance}}).then(function(response){
+				_this.person=response.data;
+				console.log(_this.person)
+				var personList=[];
+				_this.person.forEach(function(i,index,array){
+					var arrUser=i.userid
+					personList.push(arrUser);
 				})
 				
+				$(".content ul").on("click",$(".content li"),function(ev){
+					var ev = ev || window.event;
+					var target = ev.target || ev.srcElement;
+					if(target.nodeName.toLowerCase() == "li"){
+						if ($(target).children().hasClass('bg')) {
+							$(target).children('.bg').remove();
+							$(target).removeAttr('data-id');
+						}else{
+							var lis=document.createElement('span')
+							lis.setAttribute("class","bg");
+							lis.innerHTML="√";
+							if(target.nodeName.toLowerCase() == "li"){
+								var ind=$(".content li").index($(target));
+								$(target).append(lis).attr('data-id',personList[ind]);
+							}
+						}
+					}
+				})
+			},function(err){
+				console.log(err)
+			});
+		},
+		/*点击确定*/
+		showok:function(){
+			this.showuser=!this.showuser;
+			var _this=this;
+			var lis=$(".content li")
+			var personArr=[];
+			for(var i=0;i<lis.length;i++){
+				if(lis.eq(i).attr('data-id')!=undefined){
+					personArr.push(lis.eq(i).attr('data-id'));
+				}
 			}
+			_this.personStr=personArr.join(",")
+			//console.log(_this.personStr)
+			if($(".bg").length=="0"){
+				layer.open({
+				    content: '请添加选择人',
+				    skin: 'msg',
+				    style: 'background-color:#ccc; color:#fff; border:none;',
+				    time: 1
+				});
+			}
+		},
+			
+		/*常用语*/
+		lane:function(){
+			var _this=this;
+			var user=getCookie('userid');
+			axios.get("/workflow/getCommonlanguage",{params:{userid:user}}).then(function(response){
+    			console.log(response.data)
+        		/*_this.language=response.data
+        		console.log(_this.language)*/
+    			var html='';
+    			$.each(response.data,function(i,val){
+    				html+='<option class="option" value="'+response.data[i].phrase+'">'+response.data[i].phrase+'</option>'
+    			});
+    				var L=layer.open({
+    					type: 1,
+    					anim: 'up',
+    					style: 'position:fixed; bottom:0; left:0; width: 100%; height: 200px; padding:10px 0; border:none;box-shadow: 0;',
+    					content: html+"<p class='option'>取消</p>"
+    			})
+    			$(document).on("click","option",function(){  
+    				var value=$(this).val()
+    				console.log(value)
+    				$("#sele").val(value)
+    				layer.close(L)
+    			}); 
+				$(document).on("click","p",function(){  
+    				layer.close(L)
+    			}); 
+        		
+        	},function(err){
+        		console.log(err)
+        	})
+		},
+		/*点击提交*/
+		submit:function(){
+			/*if(this.opinion=="" || this.activeName==""){
+				layer.open({
+				    content: '输入不能为空',
+				    skin: 'msg',
+				    style: 'background-color:#ccc; color:#fff; border:none;',
+				    time: 3 
+				  });
+			}*/
+			var _this=this;
+			var str=this.opinion;
+			var parm=this.parms;
+			var comebacks=this.comeback;
+			console.log(comebacks)
+			var formtypes=this.digital.formtype;
+			var taskId=this.digital.taskid;
+			var personStrs=this.personStr;
+			var nonames=this.noname;
+			console.log(nonames)
+			var user=getCookie('userid')
+			axios.post("/workflow/processProcessing",{userid:user,taskid:taskId,nextopermap:nonames,actionname:parm.name,remark:str,actiontype:parm.type,actionvalue:parm.value,comeback:comebacks,formtype:formtypes,receiveuserids:personStrs}).then(function(response){
+				console.log(response.data)
+				console.log(response.data[0].code)
+				if(response.data[0].code=='1'){
+					layer.open({
+					    content: '提交成功',
+					    skin: 'msg',
+					    style: 'background-color:#ccc; color:#fff; border:none;',
+					    time: 3 
+					  });
+					window.location.href='agency.html';
+				}else{
+					layer.open({
+					    content: response.data[0].result,
+					    style: 'background-color:#ccc; color:#fff; border:none;',
+					    skin: 'msg',
+					    time: 3 
+					  });
+				}
+			}).catch(function(response){
+				console.log(response)
+				layer.open({
+				    content: response.result,
+				    style: 'background-color:#ccc; color:#fff; border:none;',
+				    skin: 'msg',
+				    time: 3 
+				  });
+			})
+			
 		}
-
-	})
+	}
+})
